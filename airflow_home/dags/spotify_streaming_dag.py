2a9a903b819843a7e60fb6ea2a14124cdf238457
Sneha cm <snehaanilkumar1311@gmail.com>
2026-01-19 21:58:18 +0530

    Update producer and spark consumer logic

--- airflow_home/dags/spotify_streaming_dag.py
+++ airflow_home/dags/spotify_streaming_dag.py
@@ -11,7 +11,7 @@ with DAG(
     dag_id="spotify_streaming_pipeline",
     default_args=default_args,
     start_date=datetime(2025, 1, 1),
-    schedule_interval="@hourly",
+    schedule="@hourly",
     catchup=False
 ) as dag:
 
@@ -21,11 +21,13 @@ with DAG(
     )
 
     run_spark = BashOperator(
-        task_id="run_spark_consumer",
-        bash_command="""
+    task_id="run_spark_consumer",
+    bash_command="""
         spark-submit \
+        --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.4,org.postgresql:postgresql:42.7.3 \
         /home/sneha/spotify_airflow_project/scripts/spark_consumer_airflow.py
         """
-    )
+)
+
 
     run_producer >> run_spark
